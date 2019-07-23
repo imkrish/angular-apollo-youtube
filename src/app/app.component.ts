a0f2a25a9e1f34dd9a848e0e7d4225d44239cee2
Keerati Limkulphong <imkrish.developer@gmail.com>
2019-07-23 22:52:10 +1200

    Mock features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Todo } from "../generated/graphql";
+import { debounce } from "lodash";
 
 @Component({
   selector: "app-root",
@@ -23,4 +24,49 @@ export class AppComponent {
       updatedAt: new Date()
     }
   ];
+
+  onAddTodo(addTodoInput: HTMLInputElement) {
+    this.todoList = this.todoList.concat({
+      _id: Date.now().toString(),
+      todo: addTodoInput.value,
+      completed: false,
+      updatedAt: new Date(),
+      createdAt: new Date()
+    });
+    addTodoInput.value = "";
+  }
+
+  onDeleteTodo(todo: Todo) {
+    this.todoList = this.todoList.filter(
+      existingTodo => existingTodo._id !== todo._id
+    );
+  }
+
+  onToggleCompleted(todo: Todo) {
+    const foundTodoIdx = this.todoList.findIndex(
+      existingTodo => existingTodo._id === todo._id
+    );
+
+    const foundTodo = this.todoList[foundTodoIdx];
+
+    this.todoList = [
+      ...this.todoList.slice(0, foundTodoIdx),
+      { ...foundTodo, completed: !foundTodo.completed },
+      ...this.todoList.slice(foundTodoIdx + 1)
+    ];
+  }
+
+  onChangeTodo = debounce((todo: Todo, newValue: string) => {
+    const foundTodoIdx = this.todoList.findIndex(
+      existingTodo => existingTodo._id === todo._id
+    );
+
+    const foundTodo = this.todoList[foundTodoIdx];
+
+    this.todoList = [
+      ...this.todoList.slice(0, foundTodoIdx),
+      { ...foundTodo, todo: newValue },
+      ...this.todoList.slice(foundTodoIdx + 1)
+    ];
+  }, 500);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from "./app.component";
+import { GraphQLModule } from "./graphql.module";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    GraphQLModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, GraphQLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
